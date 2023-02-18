117b77c93542ebf384071591cf54215e688e53c7
dungdunge <yoon8720@nate.com>
2023-02-18 21:47:56 +0900

    dataframe02

--- dataframe.R
+++ dataframe.R
@@ -1,6 +1,6 @@
 No <- c(1,2,3,4)
 Name <- c("Apple", "Peach", "Banana", "Grape")
-Price <- c(500,200,100,50)
+Price <- c(100,200,300,50)
 Qty <- c(5,2,4,7)
 
 sales <- data.frame(No, Name, Price, Qty)
@@ -11,3 +11,14 @@ sales$Name
 
 sales[1,]
 sales[c(1,2),]
+
+#특정 조건에 맞는 행 추출 subset
+subset(sales, Qty > 3)
+subset(sales, Name == "Apple")
+subset(sales, Price >= 200)
+
+#특정 조건에 맞는열 추출
+sales$Name
+subset(sales, Price >= 100, select = Name)
+subset(sales, Qty >= 2, select = -Price)
+subset(sales, Qty >= 3, select = c(Name, Price))
